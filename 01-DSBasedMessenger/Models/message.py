5f914a2b4dc449831c857ae7652a263683ac56de
hyperstellar-11034 <hyperstellar11034@outlook.com>
2025-07-08 23:45:24 +0330

    Updated message.py

--- 01-DSBasedMessenger/Models/message.py
+++ 01-DSBasedMessenger/Models/message.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timezone
 
 class Message:
-    def __init__(self, sender_id, content, timestamp=None):
+    def __init__(self, sender_id, content, timestamp=None, reply_to=None):
         self.sender_id = sender_id
         self.content = content
         self.timestamp = timestamp if timestamp is not None else self.generate_timestamp()
+        self.reply_to = reply_to  
 
     def generate_timestamp(self):
         return datetime.now(timezone.utc)
@@ -16,3 +17,16 @@ class Message:
 
     def __str__(self):
         return "[{}] User {}: {}".format(self.timestamp.isoformat(), self.sender_id, self.content)
+
+    def to_dict(self):
+        return {
+            "sender_id": self.sender_id,
+            "content": self.content,
+            "timestamp": self.timestamp.isoformat(),
+            "reply_to": self.reply_to
+        }
+
+    @staticmethod
+    def from_dict(d):
+        timestamp = datetime.fromisoformat(d["timestamp"])
+        return Message(d["sender_id"], d["content"], timestamp=timestamp, reply_to=d.get("reply_to"))
